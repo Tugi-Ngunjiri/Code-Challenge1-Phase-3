9db8de1a9a0f98c474fcd484e67d3778e9b08846
tugi <blueqwinn@gmail.com>
2023-02-17 13:18:34 +0300

    twentieth commit

---
+++ Challenge.rb
@@ -0,0 +1,21 @@
+ class class_name
+
+ ## class name is Animal
+  class Animal
+
+    ## class variables
+    @@type_of_animal = 5
+    @@no_of_animal = 10
+end
+
+object_name = Class_name.new
+
+## Two Objects of  Animal class
+sanimal = Animal.new
+nanimal = Animal.new
+
+def method_name
+
+    # statements or code to be executed
+
+end
