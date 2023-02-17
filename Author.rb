bfbf498a7911f7d76fb834594a465a6738f973a7
tugi <blueqwinn@gmail.com>
2023-02-17 14:43:07 +0300

    twentifierst commit

--- Author.rb
+++ Author.rb
@@ -17,6 +17,26 @@ end
  # end of method
 end
 
-# creating object Author
-# calling method using object
+# creating object Author (Determining the number of objects being created)
+class Author
+ @@no_of_authors = 2
+end
+
+ # Creating Objects  in Ruby using new Method
+author1 = Author.new
+autthor1 = Author.new
+
+# calling method using object/ Custom Method To create objects
+class Author
+    @@no_of_Authors= 0
+    def initialize(id, name, addr)
+       @Author_id = id
+       @Author_name = name
+       @Author_addr = addr
+    end
+ #Creating Objects
+
+ Author1 = Author.new("1", "Maisy" "Tales of the fox , Random") 
+Author2 = Author.new("2", "John" "Tales of the Red Panda , Okinawa")
+
 # Creating Association and Aggregate methods
